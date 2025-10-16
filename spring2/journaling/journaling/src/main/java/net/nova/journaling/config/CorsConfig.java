3af2f2f08233570b8e541ffb8f8d3a97fd8d3230
Abhijan2004 <126558305+Abhijan2004@users.noreply.github.com>
2025-10-16 16:43:25 +0530

    Update CorsConfig.java

--- spring2/journaling/journaling/src/main/java/net/nova/journaling/config/CorsConfig.java
+++ spring2/journaling/journaling/src/main/java/net/nova/journaling/config/CorsConfig.java
@@ -16,9 +16,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8081", "http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8081", "http://127.0.0.1:5500", "http://localhost:5500","https://nova-git-main-abhijan-das-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
